79f87d0daadc9a7041c2afd9fd79fa9fa1cbb1ad
RRB-Rafael <Rafael.Bastos9928@gmail.com>
2025-04-25 12:27:56 -0300

    2504

--- ListaDCompra/app/index.js
+++ ListaDCompra/app/index.js
@@ -1,11 +1,31 @@
+import {Link, link} from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Oioi</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImage}>
+        <Image source={require('../assets/avatar.jpg')}
+        style={styles.avatar}
+        />
+      </View>
+
+      <View style={styles.content}>
+          <Text style={styles.title}>
+            Rafa's Shop List :D
+          </Text>
+          <Text style={styles.text}>
+            Monte sua lista de compras e não esqueça mais
+            o que precisa comprar ao sair de casa!
+          </Text>
+          <Link style={styles.button} href={"/home"}>
+            <Text style={styles.buttonText}>Acessar</Text>
+          </Link>
+
+        </View>
+
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -13,8 +33,52 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+  },
+
+  containerImage: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  avatar:{
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  text:{
+    fontSize: 16,
+    color: 'gray',
+  },
+  button: {
+    position: 'absolute',
+    bottom: '15%',
+    backgroundColor: '#000',
+    alignSelf: 'center',
+    borderRadius: 50, 
+    paddingVertical: 15,
+    width:'60%',
+    textAlign: 'center', 
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
