7f8719a082fc838cfe4a63897813bd799d1161e2
RRB-Rafael <Rafael.Bastos9928@gmail.com>
2025-05-16 12:27:20 -0300

    1605

--- ListaDCompra/app/home.jsx
+++ ListaDCompra/app/home.jsx
@@ -1,74 +1,84 @@
 import { Alert, FlatList, ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons';
+import ItemList from '../components/itemList';
 
 export default function Home() {
-    const [textInput, setTextInput] = useState('');
-    const [items, setItems] = useState([]);
+  const [textInput, setTextInput] =   useState('');
+  const [items, setItems] = useState([]);
 
-    const addItem = () => {
-        if (textInput == '') {
-            Alert.alert(
-                'Cê É BURRO PRR ?',
-                'Por favor, informe o nome do produto!'
-            ); 
-        } else{ 
-            const newItem = {
-                id: Date.now(),
-                name: textInput,
-                bought: false
-            }
-            setItems([...items, newItem]);
-            setTextInput('');
-            console.log(items)
-        }
+  const addItem = () => {
+    if (textInput == ''){
+      Alert.alert(
+        'Ocorreu um problema :(',
+        'Por favor, informe o nome do produto!'
+      );
+    } else {
+      const newItem = {
+        id: Date.now().toString(),
+        name: textInput,
+        bought: false
+      }
+      setItems([...items, newItem]);
+      setTextInput('');
+      //console.log(items)
     }
+  }
 
-    return (
+  const markItemBought = itemId => {
+    const newItems= items.map((item) => {
+        if (item.id == itemId) {
+            return{ ...item, bought: true}
+        }
+    });
+    setItems(newItems);
+  }
+
+  return (
     <SafeAreaView style={{flex: 1}}>
-        <ImageBackground 
-        source={require('../assets/background.jpg')}
-        style={{flex:1, justifyContent: 'flex-start'}}
-        resizeMode='repeat'
-        >
+      <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={{flex: 1, justifyContent: 'flex-start'}}
+      resizeMode='repeat'
+      >
         <View style={styles.header}>
-            <Text style={styles.title}>Lista de Produtos</Text>    
-            <Ionicons name="trash" size= {32} color= "#fff"/>
-        </View>    
+            <Text style={styles.title}>Lista de Produtos</Text>
+            <Ionicons name="trash" size={32} color="#fff" />
+        </View> 
 
-        <FlatList
-            contentContainerStyle= {{padding: 20, paddingBottom: 100, color:'#fff'}} 
-            data={items}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => 
-                <Text style={{color: "#fff", fontSize: 30}} >{item.name}</Text>
-            }
+        <FlatList 
+          contentContainerStyle={{padding: 20, paddingBottom: 100, color:'#fff'}}
+          data={items}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => 
+            <ItemList item={item}/>
+          }
         />
 
-        <View style= {styles.footer}>
-            <View style={styles.inputContainer}>
-                <TextInput 
-                color="#fff" 
-                fontSize={18}
-                placeholder='Digite o nome do produto...'
-                placeholderTextColor='#aeaeae' 
-                value={textInput}
-                onChangeText={(text) => setTextInput(text)}
-                />
-            </View>
-            <TouchableOpacity style={styles.iconContainer}>
-                <Ionicons name='add' size={36} color="#fff"/>
-            </TouchableOpacity>
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              color="#fff"
+              fontSize={18} 
+              placeholder='Digite o nome do produto...'  
+              placeholderTextColor='#aeaeae'
+              value={textInput}
+              onChangeText={(text) => setTextInput(text)}
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem} >
+            <Ionicons name='add' size={36} color='#fff' />
+          </TouchableOpacity>
         </View>
 
-        </ImageBackground>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-    header:{
-        padding:25,
+    header: {
+        padding: 25,
         paddingTop: 50,
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -78,42 +88,42 @@ const styles = StyleSheet.create({
         borderBottomRightRadius: 30,
     },
 
-    title:{
+    title: {
         fontSize: 26,
         fontWeight: 'bold',
-        color: '#fff'
+        color: '#fff',
     },
 
-    footer:{
-        position: 'absolute',
-        bottom: 0,
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        backgroundColor: '#000000c0',
-        borderTopLeftRadius: 30,
-        borderTopRigthRadius: 30,
+    footer: {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      backgroundColor: '#000000c0',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
     },
 
-    inputContainer:{
-        backgroundColor: "#000",
-        elevation: 40,
-        flex: 1,
-        height: 50, 
-        marginVertical: 20, 
-        borderRadius: 30, 
-        paddingHorizontal: 20,
-        justifyContent: 'center',
+    inputContainer: {
+      backgroundColor: "#000",
+      elevation: 40,
+      flex: 1,
+      height: 50,
+      marginVertical: 20,
+      borderRadius: 30,
+      paddingHorizontal: 20,
+      justifyContent: 'center',
     },
 
-    iconContainer:{
-        height: 50,
-        width: 50,
-        backgroundColor: '#000', 
-        borderRadius: 25,
-        elevation: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
+    iconContainer: {
+      height: 50,
+      width: 50,
+      backgroundColor: '#000',
+      borderRadius: 25,
+      elevation: 40,
+      justifyContent: 'center',
+      alignItems: 'center'
     }
 })
